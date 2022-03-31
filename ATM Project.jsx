681e6c9080234d38dddc5920192d877ddcd26148
Martin Grajeda Jr <martingrajedajr@gmail.com>
2022-03-31 09:52:10 -0500

    Add files via upload

--- ATM Project.jsx
+++ ATM Project.jsx
@@ -2,7 +2,7 @@ const ATMDeposit = ({ onChange, isDeposit, isValid }) => {
   const choice = ['Deposit', 'Cash Back'];
   console.log(`ATM isDeposit: ${isDeposit}`);
   return (
-    <label className="label huge">
+    <label className="label-huge">
       <h3> {choice[Number(!isDeposit)]}</h3>
       <input id="number-input" type="number" width="200" onChange={onChange}></input>
       <input type="submit" width="200" value="Submit" id="submit-input" disabled= {!isValid}></input>
